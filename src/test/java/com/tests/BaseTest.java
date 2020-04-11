db7278d2b027444df45449444680048c155a3a67
Daniel Buitrago <daniel.buitrago@wnco.com>
2020-04-10 21:25:21 -0500

    Added configuration to use remoteWebDriver

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -1,18 +1,36 @@
 package com.tests;
 
-import com.config.DriverManagerFactory;
-import com.config.DriverType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BaseTest {
 
-    protected WebDriver driver; // children class access
+    protected WebDriver driver;
 
     @BeforeTest
-    public void setupDriver() {
-        this.driver = DriverManagerFactory.getDriverManager(DriverType.CHROME).getWebDriver();
+    public void setupDriver() throws MalformedURLException {
+
+        // BROWSER : [chrome, firefox, etc..]
+        // HUB_HOST : [localhost, IP (10.0.0.2), hostname]
+
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+
+        if (System.getProperty("BROWSER") != null && System.getProperty("BROWSER").equalsIgnoreCase("Firefox")) {
+            capabilities = DesiredCapabilities.firefox();
+        }
+
+        String host = System.getProperty("HUB_HOST") == null ?  "localhost" : System.getProperty("HUB_HOST");
+
+        StringBuilder sbUrl = new StringBuilder("http://");
+        sbUrl.append(host).append(":").append("4444").append("/wd/hub");
+
+        this.driver = new RemoteWebDriver(new URL(sbUrl.toString()), capabilities);
     }
 
     @AfterTest
